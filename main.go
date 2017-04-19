dcabc048f93feef1c99dab83e278b04d17b32470
sirrah23 <hseth93@gmail.com>
2017-04-19 00:15:26 -0400

    Changed int channel to a struct channel for input to data connection

--- main.go
+++ main.go
@@ -7,19 +7,24 @@ import "strconv"
 import "os"
 import "io/ioutil"
 
+type data_conn_info struct {
+	code int
+	info string
+}
+
 func GenerateMsgStr(code int, msg string) string {
 	//TODO: Sprintf instead
 	return strconv.Itoa(code) + " " + msg + "\r\n"
 }
 
-func passive_handler(conn net.Conn, c chan int) {
+func passive_handler(conn net.Conn, data_conn_ch chan data_conn_info) {
 	ln, err := net.Listen("tcp", "127.0.0.1:0")
 	if err != nil {
 		response := GenerateMsgStr(500, "Cannot establish a port for data retrieval")
 		conn.Write([]byte(response))
 	}
 	address := port_split_str(ln.Addr().String())
-	go data_handler(ln, c)
+	go data_handler(ln, data_conn_ch)
 	response := GenerateMsgStr(227, "Entering Passive Mode ("+address+")")
 	conn.Write([]byte(response))
 }
@@ -45,7 +50,7 @@ func main() {
 func connection_handler(conn net.Conn) {
 	successful_connection(conn)
 	rcvB := make([]byte, 1024)
-	c := make(chan int)
+	data_conn_ch := make(chan data_conn_info)
 	for {
 		n, err := conn.Read(rcvB)
 		if err != nil {
@@ -59,11 +64,11 @@ func connection_handler(conn net.Conn) {
 			end_connection(conn)
 			return
 		}
-		input_handler(words, c, conn)
+		input_handler(words, data_conn_ch, conn)
 	}
 }
 
-func input_handler(input []string, c chan int, conn net.Conn) {
+func input_handler(input []string, data_conn_ch chan data_conn_info, conn net.Conn) {
 	if input[0] == "USER" {
 		login_handler(input, conn)
 	} else if strings.Compare(input[0], "SYST") == 0 {
@@ -73,9 +78,9 @@ func input_handler(input []string, c chan int, conn net.Conn) {
 	} else if strings.Compare(input[0], "CWD") == 0 {
 		cwd_handler(input, conn)
 	} else if strings.Compare(input[0], "PASV") == 0 {
-		passive_handler(conn, c)
+		passive_handler(conn, data_conn_ch)
 	} else if strings.Compare(input[0], "LIST") == 0 {
-		ls_handler(conn, c)
+		ls_handler(conn, data_conn_ch)
 	} else {
 		syntax_err_handler(conn)
 	}
@@ -126,7 +131,7 @@ func port_address_str(host_port string) (addr_str string) {
 	return
 }
 
-func data_handler(ln net.Listener, c chan int) {
+func data_handler(ln net.Listener, data_conn_ch chan data_conn_info) {
 	conn, err := ln.Accept()
 	if err != nil {
 		fmt.Println(err)
@@ -135,10 +140,14 @@ func data_handler(ln net.Listener, c chan int) {
 		fmt.Println("Passive connection accepted!")
 	}
 	defer conn.Close()
-	<-c
-	file_list_str := get_files_dir(".")
-	conn.Write([]byte(file_list_str))
-	c <- 0
+	data_conn_req := <-data_conn_ch
+	if data_conn_req.code == 0 {
+		send_file_list(conn)
+	} else {
+		//fname := data_conn_req.info
+		//send_file(fname)
+	}
+	data_conn_ch <- data_conn_info{}
 	return
 }
 
@@ -152,12 +161,12 @@ func port_split_str(address_str string) string {
 	return address + "," + p1 + "," + p2
 }
 
-func ls_handler(conn net.Conn, c chan int) {
+func ls_handler(conn net.Conn, data_conn_ch chan data_conn_info) {
 	var response string
 	response = GenerateMsgStr(150, "File list send starting")
 	conn.Write([]byte(response))
-	c <- 0
-	<-c
+	data_conn_ch <- data_conn_info{code: 0, info: ""}
+	<-data_conn_ch
 	response = GenerateMsgStr(226, "File list send complete")
 	conn.Write([]byte(response))
 }
@@ -174,6 +183,11 @@ func get_files_dir(dir string) string {
 	}
 	CRLF := "\r\n"
 	return strings.Join(file_list, CRLF) + CRLF
+}
+
+func send_file_list(conn net.Conn) {
+	file_list_str := get_files_dir(".")
+	conn.Write([]byte(file_list_str))
 }
 
 func end_connection(conn net.Conn) {
